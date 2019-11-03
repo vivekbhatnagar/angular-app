5d8e173a31b77f5df5beea383a55afaf2e43e2f8
Vivek Bhatnagar <bhatnagar1990@gmail.com>
2019-11-03 19:33:52 +0100

    Added new warning-alert component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { WarningAlertComponent } from './warningalert/warningalert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/warningalert/warningalert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning',
+  templateUrl: './warningalert.component.html',
+  styleUrls: ['./warningalert.component.css']
+})
+export class WarningAlertComponent{}
