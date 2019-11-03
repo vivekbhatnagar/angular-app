e593a52e73e09f7267af567aebc134637c48a1e8
Vivek Bhatnagar <bhatnagar1990@gmail.com>
2019-11-03 21:59:41 +0100

    String Interpolation EventBinding PropertyBinding Two-wayBinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -12,7 +13,8 @@ import { WarningAlertComponent } from './warning-alert/warning-alert.component';
     WarningAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html'
 })
-export class ServerComponent {
+export class ServerComponent implements OnInit {
+  username = 'my-name';
+  constructor() {
+  }
+
+  ngOnInit() {}
+
+  isClickable() {
+  return this.username.length ===0;
+  }
+
+  resetUserName() {
+  this.username="";
+  }
 }
