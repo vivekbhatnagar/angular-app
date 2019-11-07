f08107e5b905073d81121ae473b6faba076b461e
Vivek Bhatnagar <bhatnagar1990@gmail.com>
2019-11-07 22:13:30 +0100

    Binded custom property and @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
+  serverElements = [{ type: 'server', name: 'A server with name', content: 'server content' }];
 
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
 
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
 }
